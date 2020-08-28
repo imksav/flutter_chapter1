766628b3589f23420fce2d3a9856e27f6f675fc8
Keshav Bhandari <imksav@gmail.com>
2020-08-28 19:43:00 +0545

    Explained container, padding, alignment, row and column

--- lib/main.dart
+++ lib/main.dart
@@ -18,30 +18,36 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
           child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.black,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
+            width: 600,
+            height: 200,
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ],
             ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-            ),
-          ],
+          ),
         ),
       )),
     );
