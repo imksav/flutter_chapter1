4782962bf2c72685de6314c10dc6733aca77fbc6
Keshav Bhandari <imksav@gmail.com>
2020-08-28 19:19:10 +0545

    Completed Chapter 2 with Container and its properties

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,40 @@ class HomePage extends StatelessWidget {
         title: Text("Chapter1"),
         backgroundColor: Colors.red,
       ),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Flutter Day 1 Chapter1"),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          // if we are using decoration, we can't use color outside the decoration; either in decoration or no any decotration
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.red,
+                blurRadius: 4.0,
+                offset: Offset.zero,
+                spreadRadius: 5.0,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(29.0),
+            gradient: LinearGradient(
+              colors: [
+                Colors.red,
+                Colors.green,
+                Colors.yellow,
+              ],
+            ),
+          ),
+          width: 100,
+          height: 100,
+          child: Text(
+            "Flutter\nDay 1\nChapter1\nCompleted",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.visible,
+            maxLines:
+                4, // due to 4 remaining text which was inserted in the line 5 was not visible
+            style: TextStyle(color: Colors.purple, fontSize: 20.0),
+          ),
         ),
       ),
     );
