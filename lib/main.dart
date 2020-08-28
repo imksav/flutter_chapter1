6d61f6b256e25790dc7789c7e6b8ca40dae1d0bc
Keshav Bhandari <imksav@gmail.com>
2020-08-28 19:39:09 +0545

    Row And Column Explained

--- lib/main.dart
+++ lib/main.dart
@@ -18,41 +18,32 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          alignment: Alignment.center,
-          // if we are using decoration, we can't use color outside the decoration; either in decoration or no any decotration
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.red,
-                blurRadius: 4.0,
-                offset: Offset.zero,
-                spreadRadius: 5.0,
-              ),
-            ],
-            borderRadius: BorderRadius.circular(29.0),
-            gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                Colors.green,
-                Colors.yellow,
-              ],
+          child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
             ),
-          ),
-          width: 100,
-          height: 100,
-          child: Text(
-            "Flutter\nDay 1\nChapter1\nCompleted",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.visible,
-            maxLines:
-                4, // due to 4 remaining text which was inserted in the line 5 was not visible
-            style: TextStyle(color: Colors.purple, fontSize: 20.0),
-          ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
