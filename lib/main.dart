e7c0d2a4ce5bae11cf711d77bab284c49623b3f4
Keshav Bhandari <imksav@gmail.com>
2020-08-29 18:00:53 +0545

    Completed drawer, floating action button and ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -18,38 +18,58 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            width: 600,
-            height: 200,
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.green,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          reverse: false,
+          children: [
+            UserAccountsDrawerHeader(
+              arrowColor: Colors.amber,
+              accountName: Text("Keshav Bhandari"),
+              accountEmail: Text("imksav@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage:
+                    NetworkImage("https://imksav.com/uploads/imksav.jpg"),
+              ),
+              otherAccountsPictures: [
+                Text("Gmail2"),
+                Text("Gmail3"),
               ],
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              subtitle: Text("Everything"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                print("Pressed Home");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("ImKsav"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("imksav@gmail.com"),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ],
         ),
-      )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.favorite),
+      ),
     );
   }
 }
